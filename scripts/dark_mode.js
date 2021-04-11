ebb07b0f03b92e1fa0a6999537bdd07d8710b5b3
workonfire <kolucki62@gmail.com>
2021-04-12 00:06:38 +0200

    Responsiveness implementation

--- scripts/dark_mode.js
+++ scripts/dark_mode.js
@@ -2,12 +2,17 @@ let darkTheme = false;
 if (localStorage.getItem('darkTheme') === null) localStorage.setItem('darkTheme', darkTheme.toString());
 else darkTheme = localStorage.getItem('darkTheme') === "true";
 
+function invertColors(className) {
+    let items = document.getElementsByClassName(className);
+    for (let i = 0; i < items.length; i++) items.item(i).classList.toggle('inverted-colors');
+}
+
 function enableDarkMode() {
     localStorage.setItem('darkTheme', darkTheme.toString());
     document.body.classList.toggle('dark-mode');
 
-    const logos = document.getElementsByClassName('logo');
-    for (let i = 0; i < logos.length; i++) logos.item(i).classList.toggle('inverted-colors');
+    invertColors('logo');
+    invertColors('link');
 
     document.getElementById('dark-mode-toggle').classList.toggle('inverted-colors');
 }
